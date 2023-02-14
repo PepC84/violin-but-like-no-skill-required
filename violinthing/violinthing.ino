9a1f2c4dadf504c2c0513a55748f3f4098e06811
Pep <57294729+PepC84@users.noreply.github.com>
2023-02-14 00:38:01 -0800

    Update violinthing.ino

--- violinthing/violinthing.ino
+++ violinthing/violinthing.ino
@@ -1,186 +1,145 @@
-
 // mp1 addresses = 
 /*  -- |pin on the circuit| |name of pin on hardware| |ID of the LED on arduino|
---  21  GPA0  0  G3
---  22  GPA1  1  G#3
---  23  GPA2  2  A3
---  24  GPA3  3  A#3
---  25  GPA4  4  B3
---  26  GPA5  5  C4
---  27  GPA6  6  C#4
---  28  GPA7  7  D4
---  1   GPB0  8  D4#
---  2   GPB1  9  E4
---  3   GPB2  10 F4
---  4   GPB3  11 F#4
---  5   GPB4  12 G4
---  6   GPB5  13 G#4
---  7   GPB6  14 A4
---  8   GPB7  15 A#4
+--  21  GPA0  0  A#4
+--  22  GPA1  1  C#5  
+--  23  GPA2  2  C5
+--  24  GPA3  3  G#5
+--  25  GPA4  4  F#5
+--  26  GPA5  5  A#5
+--  27  GPA6  6  F5
+--  28  GPA7  7  B4
+--  1   GPB0  8  G3
+--  2   GPB1  9  A5
+--  3   GPB2  10 D#5
+--  4   GPB3  11 E5
+--  5   GPB4  12 G5
+--  6   GPB5  13 A4
+--  7   GPB6  14 D4
+--  8   GPB7  15 D5
 */
 //mp2 addresses = 
 /* substract 16 for actual address
---  21  GPA0  16 B4
---  22  GPA1  17 C5
---  23  GPA2  18 C#5
---  24  GPA3  19 D5
---  25  GPA4  20 D#5
---  26  GPA5  21 E5
---  27  GPA6  22 F5
---  28  GPA7  23 F#5
---  1   GPB0  24 G5
---  2   GPB1  25 G#5
---  3   GPB2  26 A5
---  4   GPB3  27 A#5
---  5   GPB4  28 B5
---  6   GPB5  29 B#5
---  7   GPB6  30
---  8   GPB7  31
-*/
-// h,00,00,00,e ps send format
-#include <LiquidCrystal.h>
-LiquidCrystal lcd(7,8,9,10,11,12);
+--  21  GPA0  16 G4
+--  22  GPA1  17 C#4
+--  23  GPA2  18 A#3
+--  24  GPA3  19 G#4
+--  25  GPA4  20 
+--  26  GPA5  21 E4
+--  27  GPA6  22 
+--  28  GPA7  23 A3
+--  1   GPB0  24 B3
+--  2   GPB1  25 F4
+--  3   GPB2  26 C3
+--  4   GPB3  27 
+--  5   GPB4  28 G#3
+--  6   GPB5  29 
+--  7   GPB6  30 D#3
+--  8   GPB7  31 F#4
+*/ 
 
+
+// h,000,000,000,e ps send format
+// j,000,000,000,r arduino send format
+#include <SoftwareSerial.h>
+SoftwareSerial HC06(0,1);
+char data;
+String dataString = "";
 #include <Adafruit_MCP23X17.h>
 #include <Wire.h>
 
 Adafruit_MCP23X17 mcp1;
 Adafruit_MCP23X17 mcp2;
 
-
-short int ledStates [31];
-long ledTimers [31];
-int funnyVariable = 0;
-long lastResetLCD = millis();
-short ledSelected = 16;
-int  ledPattern = 3000;;
-long offTimerCheck = millis();
-long ledSelect = millis();
+String inString; 
+int inByte;
 
 int value1 = 0;
 int value2 = 0;
 int value3 = 0;
-long  timeStampReceived = millis();
-long timeStampContact = millis();
-long timeStampLCD = millis();
-long timeStampClearLCD = millis();
-boolean timeSinceNaN = false;
-String inString; 
-int inByte;
-boolean clearedLCD = false;
-boolean toggleVals = false;
-boolean firstContact = false;
+
 String  value1String;
 String  value2String;
 String  value3String;
 
+long timeStampContact = millis();
 void setup() {
-Serial.begin(9600);
-establishContact();
+   Serial.begin(9600);
+   HC06.begin(9600);
+
+
 mcp1.begin_I2C(0x21); //low low high
 mcp2.begin_I2C(0x22); //low high low
 
- for(int i=0;i<16;i++) {
+for(int i=0;i<16;i++) {
   mcp1.pinMode(i, OUTPUT);
   mcp2.pinMode(i, OUTPUT);
   if(i==15) {
     break;
   }
- }
- 
-lcd.begin(16,2);
-lcd.print("baller v1.2");
 }
 
-void loop() {  
-  if(millis() - lastResetLCD > 500) {
-    lcd.clear();
-    lastResetLCD = millis();
-  }
-    if (toggleVals == true) {
-    lcd.setCursor(0,1);
-  lcd.print(value1String + " " + value2String + " " + value3String);
-  lcd.setCursor (0,0);
-  }
-    if (Serial.available() > 0) {
-    lcd.setCursor(15,0);
-    lcd.print('a');
-    lcd.setCursor(0,0);
-    inString = Serial.readStringUntil('e');
-    
-     if(inString != "null" ){
-       lcd.setCursor(14,0);
-       lcd.print('1');
-       lcd.setCursor(0,0);
+setLEDsLow(); 
+//establishContact();
+}
 
-          if (inString.startsWith("h")){
-            lcd.setCursor(13,0);
-            lcd.print('h');
-            lcd.setCursor(0,0);
-             value1String   =    inString.substring(2,4);
-              value1 = value1String.toInt();
-             value2String   =    inString.substring(5,7); 
-              value2 =  value2String.toInt();
-             value3String   =    inString.substring(8,10); 
-              value3 =  value3String.toInt();
-             toggleVals = true;
+void loop() {
+
+mcp2.digitalWrite(15,HIGH);
+
+ 
+  if (HC06.available() > 0) {
+
+   //   HC06.write('C');
+    inString = HC06.readStringUntil('e');
+  if(inString != "null" )
+    { 
+
+
+ if (inString.startsWith("h") )                  //   si string inicia con  "h" es valida y guarda contenido en arreglo sa[]
+     {  
+
+        value1String   =    inString.substring(2,5);
+          value1 = value1String.toInt();
+        value2String   =    inString.substring(6,9); 
+          value2 =  value2String.toInt();
+        value3String   =    inString.substring(10,13); 
+          value3 =  value3String.toInt();
+
+          
+       
+        Serial.print("v1 ");
+         Serial.println(value1);
+          Serial.print("v2 ");
+           Serial.println(value2);
+            Serial.print("v3 ");
+             Serial.println(value3);
+
                           }
-        } else {
-        lcd.setCursor(14,0);
-        lcd.print('n');
-        lcd.setCursor(0,0);
-        }
+                         
+    }
     }
 
-  if(ledStates[ledSelected] > 0 && millis() - ledTimers[ledSelected] >= ledStates[ledSelected])  {
-    turnOffLED(ledSelected);
-  }
-  if(ledStates[ledSelected] < 0 && millis() - offTimerCheck > 10)
-  {
-    offTimerCheck = millis();
-    ledStates[ledSelected] = ledStates[ledSelected] + 10; 
-  }
-  if(ledStates[ledSelected] == 0) {
-    turnLED2sec(ledSelected);
-  }
-}
-void turnOffLED (int ledID) {
-     short int actualLedID;
-    if(ledID >= 16) {
-      actualLedID = ledID - 16;
-      mcp2.digitalWrite(actualLedID, LOW) ;
-      ledTimers[ledID] = millis();
-      ledStates[ledID] = -50;
-    } else {
-      mcp1.digitalWrite(ledID , LOW);
-      ledTimers[ledID] = millis();
-      ledStates[ledID] = -50;
-     }
 
 }
-void turnLED2sec (int ledID) {
-  short int actualLedID;
-    if(ledID >= 16) {
-      actualLedID = ledID - 16;
-      mcp2.digitalWrite(actualLedID, HIGH) ;
-      ledStates[ledID] = 1000;
-      ledTimers[ledID] = millis();
-    } else {
-      mcp1.digitalWrite(ledID,HIGH);
-      ledStates[ledID] = 1000;
-      ledTimers[ledID] = millis();
-     }
+/*void establishContact() {
 
-}
-void establishContact() {
- while (Serial.available() <= 0) {
+ while (HC06.available() <= 0) {
   if (millis() - timeStampContact > 3000)
  {
-      Serial.write('A');   // send a capital A
-      lcd.setCursor(15,1);
-      lcd.print('A');
-      lcd.setCursor(0,0);
+      HC06.write('A');   // send a capital A
     timeStampContact = millis();
   }
  }
 }
+*/
+void setLEDsLow() {
+     for(int i=0;i<16;i++) {
+mcp1.digitalWrite(i,LOW);
+mcp2.digitalWrite(i,LOW);
+  if(i==15) {
+    break;
+  }
+ }
+}
+
+
